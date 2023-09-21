0b8a2d14ba6985fd39767338c16730b89efe45b0
YKyungmin <xcv556865@gmail.com>
2023-09-21 16:27:14 +0900

    d_5

--- main.js
+++ main.js
@@ -1,45 +1,61 @@
-//이미지를 동적으로 200 개 생성
-//이미지 소스 로딩이 될때가 에러가  발생하는 시스템 이벤트 설정
-//브라우저에서 마우스 움직일때마다 마우스 좌표값 구하기
-//특정 수치값을 백분율화 하는 로직 처리
-//이미지소스가 모두 로딩되는 상태를 백분율로 변환
-
-//img 노드 생성
-//속성 노드 생성
-//src 속성 노드에 value = img/pic0100.jpg
-//위의 100번을 반복을 돌리면서
-//append로 이미지 요소 반복 추가
-
-//백분율 구하는 공식
-
-//현재 수치값 / 전체 수치값 * 100 (백분율)
-//현재 수치값 / 전체 수치값 * 200 (2백분율)
-
+const num = 200;
 const section = document.querySelector('section');
-const imgs = createImgs(section, 200);
-console.log(imgs);
+const aside = document.querySelector('aside');
+const loadingNum = document.querySelector('aside p span');
+const imgs = createImgs(section, num);
 
 window.addEventListener('mousemove', (e) => {
-	const curPos = e.pageX;
-	const wid = window.innerWidth;
-	const percent = parseInt((curPos / wid) * 200);
-
-	console.log(percent);
-	//parsInt(숫자) : 실수에서 소수점 아래를 버려서 정수 반환
-	//parseFloat(숫자) : 소수점 아래까지 있는 실수 반환
+	const percent = getPercent(e, num);
+	activation(imgs, percent);
 });
 
-//인수를 갯수로 받아서 동적으로 img 생성해주는 함수
+function getPercent(e, num) {
+	const curPos = e.pageX;
+	const wid = window.innerWidth;
+	return parseInt((curPos / wid) * num);
+}
+
 function createImgs(target, num) {
-	for (let ret = 0; ret < 200; ret++) {
+	for (let i = 0; i < num; i++) {
 		const img = document.createElement('img');
 		const src = document.createAttribute('src');
-
-		src.value = `img/pic${ret}.jpg`;
-
+		src.value = `img/pic${i}.jpg`;
 		img.setAttributeNode(src);
-
 		target.append(img);
 	}
-	return target.querySelectorAll('img');
+	const imgs = target.querySelectorAll('img');
+	let count = 0;
+	imgs.forEach((img) => {
+		//해당 돔에 수반되는 소스이미지가 로딩완료시 실행되는 이벤트
+		img.onload = () => {
+			count++;
+			const percent = parseInt((count / num) * 100);
+			loadingNum.innerText = percent;
+			console.log('현재 로딩된 소스이미지', count);
+			if (count === num) {
+				//동적으로 만들어진 img요소의 소스이미지가 렌더링완료된 시점
+				console.log('모든 소스이미지 로딩 완료');
+				aside.classList.add('off');
+				setTimeout(() => {
+					aside.remove();
+				}, 2000);
+			}
+		};
+	});
+	return imgs;
+}
+
+function activation(arr, index) {
+	arr.forEach((el) => (el.style.display = 'none'));
+	arr[index].style.display = 'block';
+}
+
+//인수로 transitionDuration값을 구해야 하는 DOM요소를 전달 받음
+function convertSpeed(el) {
+	//해당요소에 transitionDuration 값을 재연산 해서 가져온다음
+	//숫자로 바꾼후 *1000을 해서 밀리세컨드 형태로 반환
+
+	const result = getComputedStyle(el).transitionDuration;
+	const result2 = parseFloat(result);
+	console.log(result);
 }
